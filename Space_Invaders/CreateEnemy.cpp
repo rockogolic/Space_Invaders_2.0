7d2242775587c0c8a967f16d7b96c8405ab8c897
goger <rockogolic@gmail.com>
2020-05-20 14:18:07 +0200

    <class CreateEnemy> cosmetic

--- Space_Invaders/CreateEnemy.cpp
+++ Space_Invaders/CreateEnemy.cpp
@@ -7,14 +7,6 @@ CreateEnemy::CreateEnemy(Vector2i number, Enemy& enemy, RenderWindow& window) : 
 		_dim.y = abs(number.y) % 6;
 	}
 
-	// will error by _dim.x or _dix.y =1.
-	// Make an if statement to set dx to a new value in that case -> screen.x / 2 
-
-	//_playground.x = window.getSize().x - 70.0f;
-	//_playground.y = window.getSize().y - 200.0f;
-
-	bool changeY = false;
-
 	// size of the screen given to enemies to spawn
 	_playground.x = static_cast<float>(window.getSize().x);
 	_playground.y = static_cast<float>(window.getSize().y - 200);
@@ -58,62 +50,7 @@ void CreateEnemy::getActive() {
 
 }
 
-/*
-
-void CreateEnemy::MoveClassic(RenderWindow * window, float deltaTime) {
-	for (unsigned int i = 0; i < size(Enemies); i++) {
-		if (Enemies[i].enemy_side == Enemy::_side::LEFT) {
-			Enemies[i].sprite.setPosition(Enemies[i].sprite.getPosition().x, Enemies[i].sprite.getPosition().y + 2*Enemies[i].sprite.getTexture()->getSize().y);
-		}
-		else if (Enemies[i].enemy_side == Enemy::_side::RIGHT) {
-			Enemies[i].sprite.setPosition(Enemies[i].sprite.getPosition().x, Enemies[i].sprite.getPosition().y + 2 * Enemies[i].sprite.getTexture()->getSize().y);
-		}
-	}
-}
-
-*/
-
-
 void CreateEnemy::MoveClassic(RenderWindow* window, float deltaTime) {
-
-	/*
-	CreateEnemy - Enemies (matrix)
-	if any of the enemies[i] reached side::LEFT
-	then change the y position for every enemy and let it move further. (Still a normal Move function that belongs to enemy class: Move(&window, deltaTime);
-
-
-	the move in main is:
-	for (i in .. CreateEnemy)
-	enemy[i].move
-	if( enemy[i].side:: .... does not work like that) move has to be defined in class CreateEnemy
-
-
-	------------------
-
-	given the nxn matrix of enemies  (Enemies)
-
-	for (i in size(enemies))
-		if ( enemies[i] reached right side of the screen )
-			set move LEFT
-			bool changeY = true;
-			break;
-		else if (enemies[i] reached the left side)
-			set move RIGHT
-			bool changeY = true;
-			break;
-		
-	for (i in ...)
-		if (changeY == true)
-			Enemies[i].sprite.changePosition(x, y+2y))
-			changeY = false;
-		if (side::RIGHT or side::NONE)
-			Enemies[i].sprite.move();
-		else if (side::LEFT)
-			Enemies[i].sprite.move();
-			
-
-
-	*/
 
 	for (unsigned int i = 0; i < size(Enemies); i++) {
 		if ( (Enemies[i].sprite.getPosition().x >= (window->getSize().x - Enemies[i].sprite.getTexture()->getSize().x / 2.0f)) 
@@ -158,49 +95,7 @@ void CreateEnemy::MoveClassic(RenderWindow* window, float deltaTime) {
 		}
 
 	}
-
-
-
-
-
-
 }
-
-/*
-void CreateEnemy::MoveClassic(RenderWindow * window, float deltaTime) {
-	for (unsigned int i = 0; i < size(Enemies); i++) {
-		Vector2f position = Enemies[i].sprite.getPosition();	// update vector2f _position with the latest position
-
-		if (Enemies[i].sprite.getPosition().x >= (window->getSize().x - Enemies[i].sprite.getTexture()->getSize().x / 2.0f)) {
-
-			position.y += Enemies[i].sprite.getTexture()->getSize().y;
-			Enemies[i].sprite.setPosition((window->getSize().x - Enemies[i].sprite.getTexture()->getSize().x / 2.0f), position.y);
-			side = _side::LEFT;
-
-		}
-		else if (Enemies[i].sprite.getPosition().x <= (Enemies[i].sprite.getTexture()->getSize().x / 2.0f)) {
-
-			position.y += Enemies[i].sprite.getTexture()->getSize().y;
-			Enemies[i].sprite.setPosition((Enemies[i].sprite.getTexture()->getSize().x / 2.0f), position.y);
-			side = _side::RIGHT;
-		}
-
-		if (Enemies[i].isActive()) {
-			if (side == _side::RIGHT || side == _side::NONE) {
-				Enemies[i].sprite.move(100 * deltaTime , 0);
-			}
-			else if (side == _side::LEFT) {
-				Enemies[i].sprite.move(-100 * deltaTime, 0);
-			}
-			if (Enemies[i].sprite.getPosition().y >= (window->getSize().y)) {
-				Enemies[i].setWin();
-				Enemies[i].setInactive();
-			}
-		}
-	}
-}
-//*/
-
 
 //CreateEnemy::CreateEnemy(Boss& boss) {}
 
