abb7ff78ead1d9191d1c9ce3f5b1a62f894dcb3a
rockogolic <rockogolic@gmail.com>
2020-05-24 20:17:21 +0200

    Cosmetic

--- Space_Invaders/Enemy.cpp
+++ Space_Invaders/Enemy.cpp
@@ -5,7 +5,7 @@
 std::random_device rd;  //Will be used to obtain a seed for the random number engine
 std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
 std::uniform_int_distribution<> dis(1, 10000);
-std::uniform_int_distribution<> dis2(1, 400000);
+std::uniform_int_distribution<> dis2(1, 80000);
 
 Enemy::Enemy(const Texture* texture, const Texture * textureShot) {
 	
